b893bc2e911cba33b71f8d230bedd31af3069af7
Lu-Pereira <luci.lv14@gmail.com>
2021-05-19 00:14:33 -0300

    metodo delete.

--- src/app/courses/course.service.ts
+++ src/app/courses/course.service.ts
@@ -9,24 +9,28 @@ import { Course } from "./course";
 
 export class CourseService {
 
-  private courseUrl: string = 'http://localhost:3100/api/courses';
+  private coursesUrl: string = 'http://localhost:3100/api/courses';
 
   constructor(private httpClient: HttpClient) { }
 
   retrieveAll(): Observable<Course[]> {
-    return this.httpClient.get<Course[]>(this.courseUrl);
+    return this.httpClient.get<Course[]>(this.coursesUrl);
   }
 
   retrieveById(id: number): Observable<Course> {
-    return this.httpClient.get<Course>(`${this.courseUrl}/${id}`);
+    return this.httpClient.get<Course>(`${this.coursesUrl}/${id}`);
   }
 
   save(course:  Course): Observable<Course> {
     if(course.id) {
-      return this.httpClient.put<Course>(`${this.courseUrl}/${course.id}`, course);
+      return this.httpClient.put<Course>(`${this.coursesUrl}/${course.id}`, course);
     } else {
-      return this.httpClient.post<Course>(`${this.courseUrl}`, course);
+      return this.httpClient.post<Course>(`${this.coursesUrl}`, course);
     }
+  }
+
+  deleteById(id: number): Observable<any> {
+    return this.httpClient.delete<any>(`${this.coursesUrl}/${id}`);
   }
 }
 
