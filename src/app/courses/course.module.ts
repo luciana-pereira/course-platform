78e1eea7b28c636d392342aff623fd19d82254eb
Lu-Pereira <luci.lv14@gmail.com>
2021-05-21 23:25:02 -0300

    modulos e componentes

--- src/app/courses/course.module.ts
+++ src/app/courses/course.module.ts
@@ -1,14 +1,22 @@
+import { CommonModule } from "@angular/common";
 import { NgModule } from "@angular/core";
+import { FormsModule } from "@angular/forms";
 import { RouterModule } from "@angular/router";
+import { ReplacePipe } from "../pipe/replace.pipe";
+import { StarComponent } from "../shared/component/star/star.component";
 import { CourseInfoComponent } from "./course-info.component";
 import { CourseListComponent } from "./course-list.component";
 
 @NgModule({
   declarations: [
     CourseListComponent,
-    CourseInfoComponent
+    CourseInfoComponent,
+    ReplacePipe,
+    StarComponent
   ],
   imports: [
+    CommonModule,
+    FormsModule,
     RouterModule.forChild([
       {
         path: 'courses', component: CourseListComponent
@@ -19,4 +27,6 @@ import { CourseListComponent } from "./course-list.component";
     ])
   ]
 })
-export class CourseModule {}
+export class CourseModule {
+
+}
